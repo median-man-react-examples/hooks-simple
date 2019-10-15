4cb7167abcd5c247de903822b4d064a7db182ad7
John Desrosiers <john.desro@gmail.com>
2019-10-15 05:43:17 -0700

    App renders buttons and todos
    - uses placeholder todos
    - buttons don't do anything yet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
 
+const todos = [
+  {
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: 'fugiat veniam minus',
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: 'et porro tempora',
+    completed: true
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    completed: false
+  }
+]
+
 class App extends React.Component {
   render() {
-    return <div>App</div>
+    return (
+      <div>
+        <div>
+          <button>Posts</button>
+          <button>Todos</button>
+        </div>
+        <ul>
+          {todos.map(({ title }) => (
+            <li>{title}</li>
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
