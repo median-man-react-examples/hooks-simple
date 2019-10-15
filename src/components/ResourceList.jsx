d2d23457a4287747ce319557594eee9d3896a383
John Desrosiers <john.desro@gmail.com>
2019-10-15 07:48:00 -0700

    ResourceList fetches resources using hooks

--- src/components/ResourceList.jsx
+++ src/components/ResourceList.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export const ResourceList = ({ resource }) => (
-  <div>ResourceList: {resource}</div>
-)
+const resourceUrl = resource =>
+  `http://jsonplaceholder.typicode.com/${resource}`
+
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([])
+  const fetchResources = async resource => {
+    const { data } = await axios.get(resourceUrl(resource))
+    setResources(data)
+  }
+  useEffect(() => {
+    fetchResources(resource)
+  }, [resource])
+  return (
+    <div>
+      <ul>
+        {resources.map(({ title, id }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default ResourceList
